e839db5fd2594298d8bff0c366b8be66b0b0f63b
Беликов Вадим <dokkaebimain@mail.ru>
2024-11-22 18:31:48 +0300

    исправление sql иньекций

--- lab5.py
+++ lab5.py
@@ -32,13 +32,13 @@ def register():
     if not (login and password):
         return render_template('lab5/register.html', error='Заполните все поля')
     conn, cur = db_connect()
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute(f"SELECT login FROM users WHERE login=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', error='Такой пользователь уже существует')
     
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login, password) VALUES (%s, %s);", (login, password_hash))
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)
 
@@ -53,7 +53,7 @@ def login():
         return render_template('lab5/login.html', error='Заполните все поля')
     
     conn, cur = db_connect()
-    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user = cur.fetchone()
     if not user:
         db_close(conn, cur)
@@ -85,8 +85,8 @@ def create():
     conn, cur = db_connect()
     cur.execute("SELECT * FROM users WHERE login=%s;", (login, ))
     user_id = cur.fetchone()["id"]
-    cur.execute(f"INSERT INTO articles(user_id, title, article_text) \
-                VALUES ({user_id}, '{title}', '{article_text}')")
+    cur.execute("INSERT INTO articles(user_id, title, article_text) \
+                VALUES (%s, %s, %s);", (user_id, title, article_text))
     db_close(conn, cur)
     return redirect('/lab5')
 
@@ -97,9 +97,9 @@ def list():
     if not login:
         return redirect('/lab5/login')
     conn, cur = db_connect()
-    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    cur.execute("SELECT id FROM users  WHERE login=%s;", (login, ))
     user_id = cur.fetchone()["id"]
-    cur.execute(f"SELECT * FROM articles WHERE user_id='{user_id}';")
+    cur.execute("SELECT * FROM articles WHERE user_id=%s;", (user_id, ))
     articles = cur.fetchall()
     db_close(conn, cur)
     return render_template('/lab5/articles.html', articles=articles)
