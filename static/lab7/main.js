0afaa903dc156df1869556dc87b2c127689eac6c
Беликов Вадим <dokkaebimain@mail.ru>
2024-12-26 17:31:56 +0300

    редактирование 2

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -8,26 +8,36 @@ function fillFilmList() {
         tbody.innerHTML = '';
         for(let i = 0; i<films.length; i++) {
             let tr = document.createElement('tr');
+
             let tdTitle = document.createElement('td');
             let tdTitleRus = document.createElement('td');
             let tdYear = document.createElement('td');
             let tdActions = document.createElement('td');
+
             tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title;
             tdTitleRus.innerText = films[i].title_ru;
             tdYear.innerText = films[i].year;
+
             let editButton = document.createElement('button');
             editButton.innerText = 'редактировать';
+            editButton.onclick = function() {
+                editFilm(i);
+            };
+
             let delButton = document.createElement('button');
             delButton.innerText = 'удалить';
             delButton.onclick = function() {
                 deleteFilm(i, films[i].title_ru);
             };
+
             tdActions.append(editButton);
             tdActions.append(delButton);
+
             tr.append(tdTitle);
             tr.append(tdTitleRus);
             tr.append(tdYear);
             tr.append(tdActions);
+
             tbody.append(tr);
         }
     })
@@ -36,20 +46,24 @@ function fillFilmList() {
 function deleteFilm(id, title) {
     if(! confirm(`Вы точно хотите удалить фильм "${title}"?`))
         return;
+
     fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
         .then(function () {
             fillFilmList();
         });
 }
+
 function showModal() {
     document.querySelector('div.modal').style.display = 'block';
 }
 function hideModal() {
     document.querySelector('div.modal').style.display = 'none';
 }
+
 function cancel() {
     hideModal();
 }
+
 function addFilm() {
     document.getElementById('id').value = '';
     document.getElementById('title').value = '';
@@ -58,15 +72,19 @@ function addFilm() {
     document.getElementById('description').value = '';
     showModal();
 }
+
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title-ru').value,
         year: document.getElementById('year').value,
         description: document.getElementById('description').value
     }
-    const url = `/lab7/rest-api/films/`;
-    const method = 'POST';
+
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST': 'PUT';
+
     fetch(url, {
         method: method,
         headers: {"Content-Type": "application/json"},
@@ -76,4 +94,18 @@ function sendFilm() {
         fillFilmList();
         hideModal();
     })
+}
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+    .then(function (data) {
+        return data.json();
+    })
+    .then(function (film) {
+        document.getElementById('id').value = id;
+        document.getElementById('title').value = film.title;
+        document.getElementById('title-ru').value = film.title_ru;
+        document.getElementById('year').value = film.year;
+        document.getElementById('description').value = film.description;
+        showModal();
+    });
 }
